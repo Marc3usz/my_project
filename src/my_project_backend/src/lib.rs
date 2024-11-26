cf2b366a07159de80cfc83f3ce931f46af31978f
Marcel Geba <u35_margeb_lbn@technischools.com>
2024-11-26 10:50:44 +0000

    made the app run

--- src/my_project_backend/src/lib.rs
+++ src/my_project_backend/src/lib.rs
@@ -1,4 +1,15 @@
+use std::cell::RefCell;
+
+thread_local! {
+    static MSG: RefCell<Vec<String>> = RefCell::new(Vec::new());
+}
+
 #[ic_cdk::query]
-fn greet(name: String) -> String {
-    format!("Hello, {}!", name)
+fn read_msg() -> Vec<String> {
+    MSG.with(|chat| (*chat.borrow()).clone())
+}
+
+#[ic_cdk::update]
+fn add_msg(msg: String) {
+    MSG.with(|chat| chat.borrow_mut().push(msg));
 }
--- src/my_project_frontend/src/App.vue
+++ src/my_project_frontend/src/App.vue
@@ -1,15 +1,16 @@
 <script setup>
 import { ref } from 'vue';
 import { my_project_backend } from 'declarations/my_project_backend/index';
-let greeting = ref('');
+let messages = ref([]);
 
 async function handleSubmit(e) {
   e.preventDefault();
   const target = e.target;
   const name = target.querySelector('#name').value;
-  await my_project_backend.greet(name).then((response) => {
-    greeting.value = response;
-  });
+  await my_project_backend.add_msg(name);
+  await my_project_backend.read_msg().then(
+    response => { console.log(response); messages.value = response }
+  );
 }
 </script>
 
@@ -23,6 +24,8 @@ async function handleSubmit(e) {
       <input id="name" alt="Name" type="text" />
       <button type="submit">Click Me!</button>
     </form>
-    <section id="greeting">{{ greeting }}</section>
+    <ul id="chat">
+      <li v-for="item in messages">{{ item }}</li>
+    </ul>
   </main>
 </template>
